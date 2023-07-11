2813c8ec10ee7c2f4e56614dfaaec41cfc7d23dd
Ryan <rycr3278@colorado.edu>
2023-07-11 16:05:11 +0000

    tried my best

--- code/BTree.cpp
+++ code/BTree.cpp
@@ -18,13 +18,50 @@ void BTree::remove(shared_ptr<btree> root, int key){
 }
 
 shared_ptr<btree> BTree::find(shared_ptr<btree> root, int key){
-  return shared_ptr<btree>(NULL);
+  if (root == nullptr) {
+    return nullptr;
+  }
+
+  int i = 0;
+  while (i < root->num_keys && key > root->keys[i]) {
+    i++;
+  }
+
+  if (i < root->num_keys && key == root->keys[i]) {
+    return root;
+  }
+
+  if (root->is_leaf) {
+    return nullptr;
+  }
+
+  return find(root->children[i], key);
+
 }
 
 int BTree::count_nodes(shared_ptr<btree> root){
-  return 0;
+  if (root == nullptr) {
+    return 0;
+  }
+
+  int count = 1;
+  for (int i = 0; i <= root->num_keys; i++) {
+    count += count_nodes(root->children[i]);
+  }
+
+  return count;
+
 }
 
 int BTree::count_keys(shared_ptr<btree> root){
-  return 0;
+  if (root == nullptr) {
+    return 0;
+  }
+
+  int count = root->num_keys;
+  for (int i = 0; i <= root->num_keys; i++) {
+    count += count_keys(root->children[i]);
+  }
+
+  return count;
 }
